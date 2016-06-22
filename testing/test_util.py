b5731d1477685fc22fdfdf87e4fdbf2d18103af0
Aaron Cohen <cohenaa@osuosl.org>
2016-06-22 09:40:06 -0700

    Added more tests

--- testing/test_util.py
+++ testing/test_util.py
@@ -24,3 +24,56 @@ class UtilTest(unittest.TestCase):
         self.assertEqual(util.to_readable_time(60), "0h1m")
         self.assertEqual(util.to_readable_time(3600), "1h0m")
         self.assertEqual(util.to_readable_time(1000), "0h16m")
+
+    def test_fix_user_permissions(self):
+        permissions = {
+            "userzero": "0",
+            "userone": "1",
+            "usertwo": "2",
+            "userthree": "3",
+            "userfour": "4",
+            "userfive": "5",
+            "usersix": "6",
+            "userseven": "7"
+        }
+
+        fixed_permissions = {
+            "userzero": {"member": False, "spectator": False, "manager": False},
+            "userone": {"member": False, "spectator": False, "manager": True},
+            "usertwo": {"member": False, "spectator": True, "manager": False},
+            "userthree": {"member": False, "spectator": True, "manager": True},
+            "userfour": {"member": True, "spectator": False, "manager": False},
+            "userfive": {"member": True, "spectator": False, "manager": True},
+            "usersix": {"member": True, "spectator": True, "manager": False},
+            "userseven": {"member": True, "spectator": True, "manager": True}
+        }
+
+        fixed = util.fix_user_permissions(permissions)
+
+        self.assertEqual(fixed, fixed_permissions)
+
+    def test_fix_args(self):
+        args = {
+            "<angle_arg>": "value",
+            "--long-opt": "[list values]",
+            "UPPER_ARG": "True",
+            "--duration": "300",
+            "--blank-arg": None
+        }
+
+        fixed_args_nonoptional = {
+            "angle_arg": "value",
+            "long_opt": ["list", "values"],
+            "upper_arg": True,
+            "duration": 300,
+            "blank_arg": None
+        }
+
+        fixed_args_optional = dict(fixed_args_nonoptional)
+        del(fixed_args_optional["blank_arg"])
+
+        nonoptional = util.fix_args(args, False)
+        optional = util.fix_args(args, True)
+
+        self.assertEqual(nonoptional, fixed_args_nonoptional)
+        self.assertEqual(optional, fixed_args_optional)
