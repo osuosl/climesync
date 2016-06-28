11de042e1729cba11b905e673f6f4150e5f9b028
Aaron Cohen <cohenaa@osuosl.org>
2016-06-28 13:40:11 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="climesync",
+    version="0.1",
+    packages=find_packages(),
+    install_requires = [
+        "pymesync"
+    ],
+    scripts=["climesync.py"],
+    entry_points={
+        "console_scripts": [
+            "climesync = climesync:main"
+        ]
+    }
+)
